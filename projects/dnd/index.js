a26dba228c288ca9225e2c0b0c8587da09119300
Olga Markevich <pyrkoolga@gmail.com>
2022-07-07 12:35:51 +0300

    ДЗ-6 доп/начало

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -21,11 +21,38 @@ const homeworkContainer = document.querySelector('#app');
 
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+export function createDiv() {
+  const diva = document.createElement('div');
+  diva.style.backgroundColor =
+    'rgb(' +
+    Math.floor(Math.random() * 255) +
+    ', ' +
+    Math.floor(Math.random() * 255) +
+    ', ' +
+    Math.floor(Math.random() * 255) +
+    ')';
+  diva.style.width = Math.floor(Math.random() * 1000) + 'px';
+  diva.style.height = Math.floor(Math.random() * 1000) + 'px';
+  diva.style.position = 'absolute';
+  diva.style.top = Math.floor(Math.random() * 500) + 'px';
+  diva.style.left = Math.floor(Math.random() * 500) + 'px';
+  diva.draggable = true;
+
+  console.log(diva);
+  homeworkContainer.appendChild(diva);
+
+  diva.addEventListener('dragstart', function () {
+    e.dataTransfer.setData('text/plain', e.target.id);
+  });
+
+  diva.addEventListener('dragover', function () {
+    e.preventDefault();
+  });
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
 addDivButton.addEventListener('click', function () {
   const div = createDiv();
-  homeworkContainer.appendChild(div);
+  // homeworkContainer.appendChild(div);
 });
